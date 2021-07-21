577a08c64c88a2e8381f9ef67973f5031cfea3fc
Justin Hines <1746134+soupaJ@users.noreply.github.com>
2021-07-21 16:12:21 -0700

    Adds initial work for Kustomer SDK on Android
    https://knock.atlassian.net/browse/AX-699

--- android/src/main/java/com/kustomerreactnative/KustomerReactNativeModule.java
+++ android/src/main/java/com/kustomerreactnative/KustomerReactNativeModule.java
@@ -2,33 +2,75 @@ package com.kustomerreactnative;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.module.annotations.ReactModule;
+import com.kustomer.core.models.KusChatAvailability;
+import com.kustomer.core.models.KusWidgetType;
+import com.kustomer.ui.Kustomer;
+
+import kotlin.Unit;
+
 
 @ReactModule(name = KustomerReactNativeModule.NAME)
 public class KustomerReactNativeModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "KustomerReactNative";
+  public static final String NAME = "KustomerReactNative";
 
-    public KustomerReactNativeModule(ReactApplicationContext reactContext) {
-        super(reactContext);
+  public KustomerReactNativeModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+  @ReactMethod
+  public void isChatAvailable(Promise promise) {
+    WritableArray mutableArray = Arguments.createArray();
+
+    try {
+      Kustomer.Companion.getInstance().isChatAvailable(kusResult -> {
+        // Resolve with [response, null]
+        mutableArray.pushBoolean(
+          kusResult.successOr(KusChatAvailability.KUS_OFFLINE) == KusChatAvailability.KUS_ONLINE
+        );
+        mutableArray.pushNull();
+
+        promise.resolve(mutableArray);
+
+        return Unit.INSTANCE;
+      });
+    } catch (Exception ex) {
+      // Resolve with [false, exception]
+      mutableArray.pushBoolean(false);
+      mutableArray.pushString(
+        ex.getMessage()
+      );
+
+      promise.resolve(mutableArray);
     }
+  }
 
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
+  @ReactMethod
+  public void _show(String option) {
+    switch (option) {
+      case "onlyChat":
+        Kustomer.Companion.getInstance().open(KusWidgetType.CHAT_ONLY);
+        break;
+      case "chatAndKnowledgeBase":
+        Kustomer.Companion.getInstance().open(KusWidgetType.CHAT_KB);
+        break;
+      case "onlyKnowledgeBase":
+        Kustomer.Companion.getInstance().open(KusWidgetType.KB_ONLY);
+        break;
+      default:
+        Kustomer.Companion.getInstance().open(KusWidgetType.DEFAULT);
     }
-
-
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
-    }
-
-    public static native int nativeMultiply(int a, int b);
+  }
 }
